9b4d63950ef09c003572907a798f30fb3fd5ce0c
James Johnson <james@jamesjohnson.io>
2021-11-10 16:22:42 -0500

    Added some minor error handling

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import error
 from flask import Flask, jsonify, request
 from getHoldings import getHoldings
 
@@ -7,7 +8,11 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def get():
     etf = request.args.get("etf")
-    return jsonify(getHoldings(etf))
+    try:
+        list = getHoldings(etf)
+        return jsonify(list)
+    except IndexError:
+        return jsonify(data="We are unable to find the holdings for the requested ETF.", errors="Data not found."), 404
 
 
 if(__name__ == "__main__"):
